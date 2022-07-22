4887a3acbf9a1aa56eb16cb0ec06f1d1b3db1661
zhan-zw <99405013+zhan-zw@users.noreply.github.com>
2022-07-22 19:46:09 +0800

    Update get_wangyiyun_commend.py

--- get_wangyiyun_commend.py
+++ get_wangyiyun_commend.py
@@ -30,7 +30,7 @@ def get_formparams(idcode, pagesize,page,cursor=-1):
 
 
     # 定义参数
-    # offset:页面偏移量;limit最大值为100
+    # Python format格式化字符串对大括号进行转义不是用\ , 而是用大括号进行转义。如'{{{:}}}'.format("python")转义后是'{python}'
     data =f'{{"rid":"A_PL_0_{idcode}","threadId":"A_PL_0_{idcode}","pageNo":{page},"pageSize":{pagesize},"cursor":{cursor},"offset":"0","orderType":"1","csrf_token":""}}'
     param_2 = '010001'
     param_3 = '00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7b725152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932575cce10b424d813cfe4875d3e82047b97ddef52741d546b8e289dc6935b3ece0462db0a22b8e7'
@@ -54,7 +54,7 @@ def get_comment(idcode,pagesize,page,cursor=-1):
     """
     url = "https://music.163.com/weapi/comment/resource/comments/get?csrf_token="
     headers = {
-        'user-agent': '',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36',     
         'referer': f'https://music.163.com/playlist?id={idcode}',
     }
 
